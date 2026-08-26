2b4d7c7c78b7f1f6e5ddba16d19403d5abf3814e
libertyactions3-cloud <libertyactions3@gmail.com>
2026-08-26 04:29:55 -0400

    Add plain online-player command suggestions

---
+++ JustTeams-Fabric/src/main/java/eu/kotori/justTeams/commands/TeamCommandSuggestions.java
@@ -0,0 +1,14 @@
+package eu.kotori.justTeams.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.ServerCommandSource;
+
+/** Player-name suggestions without selector syntax such as @a. */
+public final class TeamCommandSuggestions {
+    private TeamCommandSuggestions() {}
+
+    public static final SuggestionProvider<ServerCommandSource> ONLINE_PLAYERS = (context, builder) ->
+            CommandSource.suggestMatching(context.getSource().getOnlinePlayerNames(), builder);
+}
