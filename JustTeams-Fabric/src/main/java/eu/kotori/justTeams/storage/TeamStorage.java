3b2b7bccbad797a4ae5692372ae3a408ee855a0e
libertyactions3-cloud <libertyactions3@gmail.com>
2026-08-29 05:34:52 -0400

    Correct TeamWarp deserialization after AutoBank migration update

--- JustTeams-Fabric/src/main/java/eu/kotori/justTeams/storage/TeamStorage.java
+++ JustTeams-Fabric/src/main/java/eu/kotori/justTeams/storage/TeamStorage.java
@@ -40,7 +40,7 @@ public final class TeamStorage {
             Team team = readTeam(teamTag);
             readBank(team, teamTag);
             readEnderChest(team, teamTag);
-            if (dataVersion < 10) migrateAutoBankDefaults(team);
+            if (dataVersion < DATA_VERSION) migrateAutoBankDefaults(team);
             manager.register(team);
         }
         if (dataVersion < DATA_VERSION) save(manager);
@@ -133,7 +133,7 @@ public final class TeamStorage {
     }
 
     private TeamLocation readLocation(NbtCompound tag) { return new TeamLocation(tag.getString("dimension").orElse("minecraft:overworld"), tag.getDouble("x", 0D), tag.getDouble("y", 0D), tag.getDouble("z", 0D), tag.getFloat("yaw", 0F), tag.getFloat("pitch", 0F)); }
-    private TeamWarp readWarp(NbtCompound tag) { TeamWarp warp = new TeamWarp(tag.getString("name").orElse("warp"), UUID.fromString(tag.getString("owner").orElseThrow()), tag.getString("world").orElse("minecraft:overworld"), tag.getDouble("x", 0D), tag.getDouble("y", 0D), tag.getDouble("z", 0D), tag.getFloat("yaw", 0F), tag.getDouble("x", 0D), tag.getFloat("yaw", 0F), tag.getFloat("pitch", 0F)); warp.setPassword(tag.getString("password").orElse("")); warp.setCost(tag.getDouble("cost", 0D)); warp.setEnabled(tag.getBoolean("enabled").orElse(true)); warp.setMembersCanUse(tag.getBoolean("membersCanUse").orElse(true)); return warp; }
+    private TeamWarp readWarp(NbtCompound tag) { TeamWarp warp = new TeamWarp(tag.getString("name").orElse("warp"), UUID.fromString(tag.getString("owner").orElseThrow()), tag.getString("world").orElse("minecraft:overworld"), tag.getDouble("x", 0D), tag.getDouble("y", 0D), tag.getDouble("z", 0D), tag.getFloat("yaw", 0F), tag.getFloat("pitch", 0F)); warp.setPassword(tag.getString("password").orElse("")); warp.setCost(tag.getDouble("cost", 0D)); warp.setEnabled(tag.getBoolean("enabled").orElse(true)); warp.setMembersCanUse(tag.getBoolean("membersCanUse").orElse(true)); return warp; }
     private TeamPlayer readMember(NbtCompound tag) {
         UUID uuid = UUID.fromString(tag.getString("uuid").orElseThrow());
         TeamRole role; try { role = TeamRole.valueOf(tag.getString("role").orElse("MEMBER")); } catch (IllegalArgumentException ignored) { role = TeamRole.MEMBER; }
